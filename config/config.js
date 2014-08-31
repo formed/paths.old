d5ecfcf0b9b7cf6f326d47adf3a18ed6612948a8
formed <arran@formed.io>
2014-08-31 18:54:57 +0100

    Config data

--- config/config.js
+++ config/config.js
@@ -7,5 +7,8 @@ module.exports = {
 		'clientID' 		: 'Y6fvaDa4wbt8ts8bg2zppkerxVn4h9o9', 											// your App ID
 		'clientSecret' 	: 'L4BgUuGTuzJgdrJ08vCBRe1pRnlH7bh1oaMA7F82aCoV57E_mbUkjzIcZKdoKFRp', 			// your App Secret
 		'callbackURL' 	: 'http://127.0.0.1:3000/auth/moves/callback'									// Callback
+	},
+	movesAPI : {
+		'url'		: 'https://api.moves-app.com/oauth/v1/user/summary/daily/2013-W09' 
 	}
 };
